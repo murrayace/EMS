fdebb163f94e59ff35ef35a8d2aa64b32cc6e71d
murrayace <149865448+murrayace@users.noreply.github.com>
2023-11-26 22:55:21 +0800

    Delete src/com/lin directory

--- src/com/lin/dao/SystemAdminDao.java
+++
@@ -1,7 +0,0 @@
-package com.lin.dao;
-
-import com.lin.entity.SystemAdmin;
-
-public interface SystemAdminDao {
-    public SystemAdmin findByUsername(String username);
-}
--- src/com/lin/service/SystemAdminService.java
+++
@@ -1,7 +0,0 @@
-package com.lin.service;
-
-import com.lin.dto.SystemAdminDto;
-
-public interface SystemAdminService {
-    public SystemAdminDto login(String username,String password);
-}
