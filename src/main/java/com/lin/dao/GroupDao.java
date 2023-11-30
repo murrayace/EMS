c9b372846045fcd699ae545430201cc920769c4c
murrayace <149865448+murrayace@users.noreply.github.com>
2023-11-30 11:37:29 +0800

    Update GroupDao.java

--- src/main/java/com/lin/dao/GroupDao.java
+++ src/main/java/com/lin/dao/GroupDao.java
@@ -7,11 +7,11 @@ import java.util.List;
 public interface GroupDao {
     public List<Group> list();
     public List<Group> search(String key, String value);
-    public List<Group> availableList();
-    public Integer subAvailable(Integer id);
-    public Integer addAvailable(Integer id);
+    public List<Group> availableList();//有空位的分公司名單
+    public Integer subAvailable(Integer id);//減少可增加人數
+    public Integer addAvailable(Integer id);//增加可增加人數
     public List<Integer> findGroupIdByBranchId(Integer id);
-    public Integer availableId();
+    public Integer availableId();//有空位的分公司ID
     public Integer deleteById(Integer id);
     public Integer save(Group group);
     public Integer update(Group group);
